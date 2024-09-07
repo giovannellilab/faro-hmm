f71bf42fb7f2644ed7f472052ae0cb53d90b5590
guillecg <guillemcg92@gmail.com>
2024-09-07 11:15:56 +0200

    Add function for extracting HMMER hits

---
+++ modules/hmm.py
@@ -0,0 +1,54 @@
+import re
+
+from io import StringIO
+
+import pandas as pd
+
+
+def get_hmm_hits(filepath: str, sep: str = "$") -> pd.DataFrame:
+
+    hmmer_colnames = [
+        "target_name",
+        "target_accession",
+        "query_name",
+        "query_accession",
+        "e_value_full_seq",
+        "score_full_seq",
+        "bias_full_seq",
+        "e_value_best_dom",
+        "score_best_dom",
+        "bias_best_dom",
+        "exp",
+        "reg",
+        "clu",
+        "ov",
+        "env",
+        "dom",
+        "rep",
+        "inc",
+        "description_of_target"
+    ]
+
+    with open(filepath, mode="r") as handle:
+        hits = handle.readlines()
+
+        # Remove first three lines
+        hits = hits[3:]
+
+        # Remove whitespaces in description of target column
+        hits = [re.sub("#\\s.*\\sID", "ID", line) for line in hits]
+
+        # Replace multiple whitespaces by separator
+        hits = [re.sub("\\s+", sep, line) for line in hits]
+
+        # Remove trailing separator
+        hits = [line.rstrip(sep) for line in hits]
+
+        # Concatenate by new line character
+        hits = "\n".join(hits)
+
+    return pd.read_csv(
+        StringIO(hits),
+        names=hmmer_colnames,
+        sep=sep
+    )
