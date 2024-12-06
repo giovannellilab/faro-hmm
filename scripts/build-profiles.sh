1d00dc2df1a8e700c2b6c7ca5b23b7575190e2ff
guillecg <guillemcg92@gmail.com>
2024-12-06 13:43:34 +0100

    Add concatenation of multiple HMM profiles

--- scripts/build-profiles.sh
+++ scripts/build-profiles.sh
@@ -4,21 +4,25 @@
 helpFunction()
 {
   echo ""
-  echo "Usage: $0 -input_file"
-  echo -e "\t-i Input file containing the sequences to align"
+  echo "Usage: $0 -d input_dir -o out_dir -p out_prefix"
+  echo -e "\t-i Input directory containing the sequences (one .fasta per HMM)"
+  echo -e "\t-o Output directory for the HMM profile"
+  echo -e "\t-p Prefix for naming the concatenated HMM file (no extension)"
   exit 1 # Exit script after printing help
 }
 
-while getopts "i:" opt
+while getopts "i:o:p:" opt
 do
   case "$opt" in
-    i ) input_file="$OPTARG" ;;
+    i ) input_dir="$OPTARG" ;;
+    o ) out_dir="$OPTARG" ;;
+    p ) out_prefix="$OPTARG" ;;
     ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
   esac
 done
 
 # Print helpFunction in case parameters are empty
-if [ -z "$input_file" ]
+if [ -z "$input_dir" ] || [ -z "$out_dir" ] || [ -z "$out_prefix" ]
 then
   echo "Some or all of the parameters are empty";
   helpFunction
@@ -26,24 +30,41 @@ fi
 
 # ---------------------------------------------------------------------------- #
 
-# Get filename without extension
-input_basename="${input_file%.*}"
-aln_file=${input_basename}_aln.fasta
-clp_file=${input_basename}_aln_clp.sto
-hmm_file=${input_basename}.hmm
+# Avoid memory leak by moving all .hmm files to another directory
+# Otherwise, it will concatenate ${out_prefix}.hmm in a loop
+indiv_dir=$out_dir/${out_prefix}_individual
+mkdir -p $indiv_dir
 
-# Align sequences
-mafft --auto --anysymbol --thread -1 $input_file > $aln_file
+files_list=$(find $input_dir/* -name "*.fasta" -type f -maxdepth 0)
 
-# Trim alignment
-clipkit $aln_file \
-  --mode kpic-smart-gap \
-  --output $clp_file \
-  --output_file_format stockholm
+for input_file in $files_list; do
 
-# Build the HMM profile
-hmmbuild \
-  -n $(basename $input_basename) \
-  -O ${clp_file}.hmmer \
-  $hmm_file \
-  $clp_file
+  # Get filename without extension
+  input_basename="${input_file%.*}"
+
+  # Define paths
+  aln_file=${input_basename}_aln
+  clp_file=${input_basename}_aln_clp.sto
+  hmm_indiv_file=$indiv_dir/$(basename $input_basename).hmm
+
+  # Align sequences
+  mafft --auto --anysymbol --thread -1 $input_file > $aln_file
+
+  # Trim alignment
+  clipkit $aln_file \
+    --mode kpic-smart-gap \
+    --output $clp_file \
+    --output_file_format stockholm
+
+  # Build the HMM profile and concatenate
+  hmmbuild \
+    -n $(basename $input_basename) \
+    -O ${clp_file}.hmmer \
+    $hmm_indiv_file \
+    $clp_file
+
+done
+
+# Concatenate all generated HMM profiles
+hmm_final_file=$out_dir/${out_prefix}.hmm
+find $indiv_dir -name "*.hmm" -type f -exec cat > $hmm_final_file {} \;
